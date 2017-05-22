6f7461255aee28c43bd48e82cdd6c84aa5c0f955
AlejandroMgn <alejandro_mag0412@hotmail.com>
2017-05-22 10:13:52 -0500

    Integración completa de Firebase
    Descarga de todos los componentes de Firebase

---
+++ Snapchat/Snapchat/iniciarSesionViewController.swift
@@ -0,0 +1,28 @@
+//
+//  iniciarSesionViewController.swift
+//  Snapchat
+//
+//  Created by Alejandro Mendoza on 22/05/17.
+//  Copyright © 2017 mendoza. All rights reserved.
+//
+
+import UIKit
+
+class iniciarSesionViewController: UIViewController {
+
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func iniciarSesionTapped(_ sender: Any) {
+    }
+
+
+}
+
